06c032aa2710f4cc58870e1163d4e97235e89d56
astreich <astreich@bu.edu>
2022-01-28 22:54:23 -0600

    making improvements

--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@ import neopixel
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-num_pixels = 20
+num_pixels = 30
 reader = SimpleMFRC522()
 pixels = neopixel.NeoPixel(board.D21, num_pixels)
 
@@ -15,13 +15,19 @@ def run():
 	t_end = time.time() + 2
 	while time.time() < t_end:
 		for x in range(0,20):
-			time.sleep(0.005)
+			time.sleep(0.01)
 			pixels[x] = (0,0,0)
 			pixels[(x+1)%20] = (0,255,0)
 			pixels[(x+2)%20] = (0,255,0)
 			pixels[(x+3)%20] = (0,255,0)
 			pixels[(x+4)%20] = (0,255,0)
 			pixels[(x+5)%20] = (0,255,0)
+	pixels.fill((0, 255, 0))
+	time.sleep(2)
+	while time.time() < t_end:
+		for x in range(0,20):
+			time.sleep(0.005)
+			pixels[x] = (0,0,0)
 	pixels.fill((0, 0, 0))
 
 try:
