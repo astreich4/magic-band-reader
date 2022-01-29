2f0a247c0f005cda6db7eb98cac9125049be1841
astreich <astreich@bu.edu>
2022-01-28 21:22:32 -0600

    adding main

---
+++ code/main.py
@@ -0,0 +1,33 @@
+import time
+import board
+import neopixel
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+num_pixels = 19
+reader = SimpleMFRC522()
+pixels = neopixel.NeoPixel(board.D21, num_pixels)
+
+
+
+try:
+	id, text = reader.read()
+	print(id)
+	print(text)
+	run()
+finally:
+	GPIO.cleanup()
+
+def run():
+	t_end = time.time() + 4
+	while time.time() < t_end
+		for x in range(0,20):
+			time.sleep(0.05)
+			pixels[(x-1)%20] = (0,0,0)
+			pixels[x] = (0,255,0)
+			pixels[(x+1)%20] = (0,255,0)
+			pixels[(x+2)%20] = (0,255,0)
+			pixels[(x+3)%20] = (0,255,0)
+			pixels[(x+4)%20] = (0,255,0)
+			pixels[(x+4)%20] = (0,0,0)
+
