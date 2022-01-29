7058d19c694b65ae77b7ed9fa315e198ad0b97aa
astreich <astreich@bu.edu>
2022-01-28 22:44:40 -0600

    fixing bugs

--- code/main.py
+++ code/main.py
@@ -4,11 +4,21 @@ import neopixel
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-num_pixels = 19
+num_pixels = 20
 reader = SimpleMFRC522()
 pixels = neopixel.NeoPixel(board.D21, num_pixels)
 
-
+def run():
+	t_end = time.time() + 4
+	while time.time() < t_end:
+		for x in range(0,20):
+			time.sleep(0.05)
+			pixels[x] = (0,0,0)
+			pixels[(x+1)%20] = (0,255,0)
+			pixels[(x+2)%20] = (0,255,0)
+			pixels[(x+3)%20] = (0,255,0)
+			pixels[(x+4)%20] = (0,255,0)
+			pixels[(x+5)%20] = (0,255,0)
 
 try:
 	id, text = reader.read()
@@ -18,16 +28,4 @@ try:
 finally:
 	GPIO.cleanup()
 
-def run():
-	t_end = time.time() + 4
-	while time.time() < t_end
-		for x in range(0,20):
-			time.sleep(0.05)
-			pixels[(x-1)%20] = (0,0,0)
-			pixels[x] = (0,255,0)
-			pixels[(x+1)%20] = (0,255,0)
-			pixels[(x+2)%20] = (0,255,0)
-			pixels[(x+3)%20] = (0,255,0)
-			pixels[(x+4)%20] = (0,255,0)
-			pixels[(x+4)%20] = (0,0,0)
 
