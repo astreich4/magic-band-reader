934e9e7fe302d0f461c184d35652ab7d0667acdf
astreich <astreich@bu.edu>
2022-03-18 00:09:34 -0500

    bug fix

--- code/rfidcontroller.py
+++ code/rfidcontroller.py
@@ -1,16 +1,13 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-GPIO.setmode(GPIO.BCM)
+
 reader = SimpleMFRC522()
 
 
 def read():
-	try:
-		id, text = reader.read()
-		print(id)
-		print(text)
-	finally:
-		GPIO.cleanup()
+	id, text = reader.read()
+	print(id)
+	print(text)
 	return True
 
